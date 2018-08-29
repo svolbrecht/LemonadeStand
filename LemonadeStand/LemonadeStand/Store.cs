f5fa51079203ae9b6036629a55c7326c12e04e4d
StevenRandallVolbrecht <svolbrecht@yahoo.com>
2018-08-28 21:17:31 -0500

    added GetLemons and GetCups methods to store class

--- LemonadeStand/LemonadeStand/Store.cs
+++ LemonadeStand/LemonadeStand/Store.cs
@@ -42,5 +42,69 @@ namespace LemonadeStand
                     break;
             }
         }
+
+        public void GetCups()
+        {
+            Console.WriteLine("Cup purchase options:\n25 cups for $.80 - Press '1'\n50 cups for $1.50 - Press '2'\n100 cups for $2.75 - Press '3'");
+            string userInput = Console.ReadLine();
+            double price = 0;
+            int quantity = 0;
+            switch (userInput)
+            {
+                case "1":
+                    price = .8;
+                    quantity = 25;
+                    break;
+                case "2":
+                    price = 1.5;
+                    quantity = 50;
+                    break;
+                case "3":
+                    price = 2.75;
+                    quantity = 100;
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid choice");
+                    GetCups();
+                    break;
+            }
+        }
+
+        public void GetLemons()
+        {
+            Console.WriteLine("Lemon purchase options\n10 Lemons for $.65 - Press '1'\n30 Lemons for $1.8\n60 Lemons for $3.5 - Press '3'");
+            string userInput = Console.ReadLine();
+            double price = 0;
+            int quantity = 0;
+            switch (userInput)
+            {
+                case "1":
+                    price = .65;
+                    quantity = 10;
+                    break;
+                case "2":
+                    price = 1.8;
+                    quantity = 30;
+                    break;
+                case "3":
+                    price = 3.5;
+                    quantity = 60;
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid choice");
+                    GetLemons();
+                    break;
+            }
+        }
+
+        public void GetCupsOfSugar()
+        {
+
+        }
+
+        public void GetIceCubes()
+        {
+
+        }
     }
 }
